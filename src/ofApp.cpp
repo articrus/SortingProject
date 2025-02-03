563dbe51b7a20f43c13840d2c48632c350a84b50
articrus <165282270+articrus@users.noreply.github.com>
2025-02-03 00:03:03 -0500

    Added a few last minute changes/documentation

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -8,7 +8,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	generateRadii();
+	generateRadii(); //Generates the first set of random radii
 }
 
 //--------------------------------------------------------------
@@ -41,19 +41,16 @@ void ofApp::keyPressed(int key){
 
 //-Generates a random number between the min and maximum and returns it
 int ofApp::generateRandNum(int min, int max) {
-	std::random_device rd;
+	std::random_device rd; //The random object
 	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> dis(min, max);
-	return dis(gen);
+	std::uniform_int_distribution<> range(min, max); //Setting the bounds
+	return range(gen);
 }
 
 //-Empties the vector and generates 5 numbers between 10-100 to fill the list
 void ofApp::generateRadii() {
 	radii.clear();
-	for (int i = 0; i < 5; i++)
-	{
-		radii.push_back(generateRandNum(minRadius, maxRadius));
-	}
+	for (int i = 0; i < 5; i++) { radii.push_back(generateRandNum(minRadius, maxRadius)); } //Generate new numbers in bounds
 }
 
 //-Sorts the vector using the bubble sort alhorithm
@@ -68,20 +65,20 @@ void ofApp::bubbleSort(vector<int>& nums)
 	}
 }
 
-//-Sorts the vector using the insertion sort alhorithm
+//-Sorts the vector using the insertion sort algorithm
 void ofApp::insertionSort(vector<int>& nums) 
 {
 	int n = nums.size();
 	for (int i = 1; i < n; ++i) {
-		int cur = nums[i];
+		int current = nums[i];
 		int j = i - 1;
 
-		while (j >= 0 && nums[j] > cur) 
+		while (j >= 0 && nums[j] > current)
 		{
 			nums[j + 1] = nums[j];
 			j = j - 1;
 		}
-		nums[j + 1] = cur;
+		nums[j + 1] = current;
 	}
 }
 
@@ -90,7 +87,7 @@ void ofApp::merge(vector<int>& nums, int left, int mid, int right)
 {
 	int n1 = mid - left + 1;
 	int n2 = right - mid;
-
+	//To hold the values we sort
 	vector<int> Left(n1), Right(n2);
 	for (int i = 0; i < n1; i++) {
 		Left[i] = nums[left + i];
@@ -100,7 +97,7 @@ void ofApp::merge(vector<int>& nums, int left, int mid, int right)
 	}
 	int i = 0, j = 0;
 	int k = left;
-
+	//Merge values from vectors back into main vector
 	while (i < n1 && j < n2) {
 		if (Left[i] <= Right[j]) {
 			nums[k] = Left[i];
@@ -112,7 +109,7 @@ void ofApp::merge(vector<int>& nums, int left, int mid, int right)
 		}
 		k++;
 	}
-
+	//Add elements if there are any extra
 	while (i < n1) {
 		nums[k] = Left[i];
 		i++;
@@ -126,40 +123,39 @@ void ofApp::merge(vector<int>& nums, int left, int mid, int right)
 	}
 }
 
-//-The main function of the merge algorithm
+//-The main function of the merge algorithm (divide and conquer)
 void ofApp::mergeSort(vector<int>& nums, int left, int right) 
 {
-	if (left >= right)
-		return;
+	if (left >= right) { return; } //If
 	int mid = left + (right - left) / 2;
-	mergeSort(nums, left, mid);
-	mergeSort(nums, mid + 1, right);
+	mergeSort(nums, left, mid); //Left side
+	mergeSort(nums, mid + 1, right); //Right side
 	merge(nums, left, mid, right);
 }
 
 //-The partition made when using the quicksort algorithm
-int ofApp::partition(vector<int>& nums, int low, int high) {
-	int pivot = nums[high];
-	int i = low - 1;
+int ofApp::partition(vector<int>& nums, int left, int right) {
+	int middle = nums[right];
+	int i = left - 1;
 	
-	for (int j = low; j <= high - 1; j++) {
-		if (nums[j] <= pivot) {
+	for (int j = left; j <= right - 1; j++) {
+		if (nums[j] <= middle) {
 			i++;
 			swap(nums[i], nums[j]);
 		}
 	}
 
-	swap(nums[i + 1], nums[high]);
+	swap(nums[i + 1], nums[right]);
 	return (i + 1);
 }
 
 //-The main quicksort algorithm
-void ofApp::quickSort(vector<int>& nums, int low, int high)
+void ofApp::quickSort(vector<int>& nums, int left, int right)
 {
-	if (low < high) {
-		int partIndex = partition(nums, low, high);
-		quickSort(nums, low, partIndex - 1);
-		quickSort(nums, partIndex + 1, high);
+	if (left < right) {
+		int partIndex = partition(nums, left, right);
+		quickSort(nums, left, partIndex - 1); //Left
+		quickSort(nums, partIndex + 1, right); //Right
 	}
 }
 
